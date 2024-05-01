f0f8536a24b8d4d1083e60960aa4d81de5ab22c6
bilguuntamir <bilguuntamir988@gmail.com>
2024-05-01 23:14:24 +0800

    qpay mobile responsive

--- src/pages/competitions/[id]/register/index.tsx
+++ src/pages/competitions/[id]/register/index.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import CompetitionLayout from "../layout";
-import { api } from "~/utils/api";
+import { RouterOutputs, api } from "~/utils/api";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { competitionRegisterSchema } from "~/utils/zod";
@@ -20,6 +20,9 @@ import { Button } from "~/components/ui/button";
 import { useSession } from "next-auth/react";
 import { Alert, AlertTitle } from "~/components/ui/alert";
 import LoadingScreen from "~/components/loading-screen";
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const defaultValues: z.infer<typeof competitionRegisterSchema> = {
   competitionId: 0,
@@ -27,11 +30,17 @@ const defaultValues: z.infer<typeof competitionRegisterSchema> = {
   guestCount: 0,
 };
 
+type InvoiceResponse = RouterOutputs["payment"]["createInvoice"];
+
 export default function CompetitionRegisterPage() {
   const utils = api.useUtils();
   const router = useRouter();
   const id = parseInt(router.query.id?.toString() || "0");
   const session = useSession();
+
+  const [qpayResponse, setQpayResponse] = useState<InvoiceResponse | null>(
+    null,
+  );
 
   const { data: competition, isLoading } = api.competition.getById.useQuery(
     id,
@@ -95,7 +104,8 @@ export default function CompetitionRegisterPage() {
   const { mutate: createInvoice, isLoading: invoiceLoading } =
     api.payment.createInvoice.useMutation({
       onSuccess(data) {
-        window.open(data.qPay_shortUrl);
+        // window.open(data.qPay_shortUrl);
+        setQpayResponse(data);
       },
       onError(error) {
         toast({
@@ -180,33 +190,63 @@ export default function CompetitionRegisterPage() {
               </FormItem>
             )}
           />
-          {session.data?.user.id ? (
-            <Button disabled={registerLoading || updateRegisterLoading}>
-              {current ? "Шинэчлэх" : "Бүртгүүлэх"}
-            </Button>
-          ) : (
-            <Alert variant="destructive">
-              <AlertTitle>Бүртгүүлэхийн тулд эхэлж нэвтэрж орно уу.</AlertTitle>
-            </Alert>
-          )}
-          {session?.data?.user.id && current && (
-            <Button
-              className="ml-2"
-              type="button"
-              disabled={invoiceLoading}
-              onClick={() =>
-                createInvoice({
-                  userId: session.data.user.id,
-                  competitorId: current.id,
-                  amount: "1000",
-                })
-              }
-            >
-              Төлбөр төлөх
-            </Button>
-          )}
+          <div className="flex justify-center md:justify-start">
+            {session.data?.user.id ? (
+              <Button disabled={registerLoading || updateRegisterLoading}>
+                {current ? "Шинэчлэх" : "Бүртгүүлэх"}
+              </Button>
+            ) : (
+              <Alert variant="destructive">
+                <AlertTitle>
+                  Бүртгүүлэхийн тулд эхэлж нэвтэрж орно уу.
+                </AlertTitle>
+              </Alert>
+            )}
+            {session?.data?.user.id && current && (
+              <Button
+                className="ml-2"
+                type="button"
+                disabled={invoiceLoading}
+                onClick={() =>
+                  createInvoice({
+                    userId: session.data.user.id,
+                    competitorId: current.id,
+                    amount: "1000",
+                  })
+                }
+              >
+                Төлбөр төлөх
+              </Button>
+            )}
+          </div>
         </form>
       </Form>
+      {qpayResponse && (
+        <div>
+          <Image
+            src={"data:image/png;base64, " + qpayResponse.qr_image}
+            height={300}
+            width={300}
+            alt="qpay"
+            className="hidden md:block"
+          />
+          <ul className="flex flex-wrap justify-center gap-x-4 gap-y-6 px-4 py-6 md:hidden">
+            {qpayResponse.urls.map((i) => (
+              <li key={i.name} className="w-1/3 md:w-auto">
+                <Link href={i.link}>
+                  <Image
+                    src={i.logo}
+                    alt={i.name}
+                    width={100}
+                    height={100}
+                    className="mx-auto w-auto rounded-lg"
+                  />
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </CompetitionLayout>
   );
 }
