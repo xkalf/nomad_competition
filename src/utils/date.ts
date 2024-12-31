5547b70191bc2eb61fdca2728c5ff4bc748cb6bc
bilguuntamir <bilguuntamir988@gmail.com>
2024-12-31 19:03:37 +0800

    deploy

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,7 @@
 import { format } from "date-fns";
 import { mn } from "date-fns/locale";
 
-export const mnFormat = (input: Date | string) => {
+export const mnFormat = (input: Date | string | number) => {
   return format(input, "yyyy-MM-dd", { locale: mn });
 };
 
